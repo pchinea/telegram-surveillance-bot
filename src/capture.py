619d6686794323ae9a2b83decfa5e2b7e0482bbf
Pablo Chinea <khertz@gmail.com>
2020-04-26 22:34:25 +0100

    Renames private attributes and methods.

--- src/capture.py
+++ src/capture.py
@@ -11,59 +11,59 @@ import numpy as np
 
 class CameraDevice:
     def __init__(self, cam_id=0):
-        self.device = cv2.VideoCapture(cam_id)
-        self.frame = self.device.read()[1]
-        self.height, self.width, _ = self.frame.shape
+        self._device = cv2.VideoCapture(cam_id)
+        self._frame = self._device.read()[1]
+        self._height, self._width, _ = self._frame.shape
 
-        self.frame_count = 0
-        self.start_time = 0.0
+        self._frame_count = 0
+        self._start_time = 0.0
 
-        self.running = False
-        self.thread = Thread(target=self.update)
-        self.lock = Lock()
+        self._running = False
+        self._thread = Thread(target=self.update)
+        self._lock = Lock()
 
     def start(self):
-        if not self.running:
-            self.frame_count = 0
-            self.start_time = time()
-            self.running = True
-            self.thread.start()
+        if not self._running:
+            self._frame_count = 0
+            self._start_time = time()
+            self._running = True
+            self._thread.start()
 
     def update(self):
-        while self.running:
-            frame = self.device.read()[1]
-            with self.lock:
-                self.frame = frame
-                self.frame_count += 1
+        while self._running:
+            frame = self._device.read()[1]
+            with self._lock:
+                self._frame = frame
+                self._frame_count += 1
 
     def read(self, with_timestamp=True) -> Tuple[int, np.ndarray]:
-        with self.lock:
-            frame_id = self.frame_count
-            frame = self.frame.copy()
+        with self._lock:
+            frame_id = self._frame_count
+            frame = self._frame.copy()
         if with_timestamp:
-            self.add_timestamp(frame)
+            self._add_timestamp(frame)
         return frame_id, frame
 
     def stop(self):
-        self.running = False
-        self.thread.join()
+        self._running = False
+        self._thread.join()
 
     @property
     def fps(self):
-        return self.frame_count / (time() - self.start_time)
+        return self._frame_count / (time() - self._start_time)
 
     @property
     def frame_size(self):
-        return self.width, self.height
+        return self._width, self._height
 
     @staticmethod
-    def add_timestamp(frame: np.ndarray):
-        dt = str(datetime.now())[:-7]
+    def _add_timestamp(frame: np.ndarray):
+        now = str(datetime.now())[:-7]
         org = (1, frame.shape[0] - 3)
         font = cv2.FONT_HERSHEY_PLAIN
         size = 1
-        cv2.putText(frame, dt, org, font, size, (0, 0, 0), 2)
-        cv2.putText(frame, dt, org, font, size, (255, 255, 255), 1)
+        cv2.putText(frame, now, org, font, size, (0, 0, 0), 2)
+        cv2.putText(frame, now, org, font, size, (255, 255, 255), 1)
 
 
 class Camera:
@@ -71,25 +71,25 @@ class Camera:
     STATUS_MOTION_DETECTED = 'motion_detected'
 
     def __init__(self, cam_id=0):
-        self.camera = CameraDevice(cam_id)
-        self.surveillance_mode = False
+        self._camera = CameraDevice(cam_id)
+        self._surveillance_mode = False
 
     def start(self):
-        self.camera.start()
+        self._camera.start()
 
     def stop(self):
-        self.camera.stop()
+        self._camera.stop()
 
     def get_photo(self) -> IO:
-        return BytesIO(cv2.imencode(".jpg", self.camera.read()[1])[1])
+        return BytesIO(cv2.imencode(".jpg", self._camera.read()[1])[1])
 
     def get_video(self, seconds=5) -> IO:
         file, video_writer = self._create_video_file('on_demand')
-        n_frames = self.camera.fps * seconds
+        n_frames = self._camera.fps * seconds
 
         processed = []
         while len(processed) < n_frames:
-            frame_id, frame = self.camera.read()
+            frame_id, frame = self._camera.read()
             if frame_id not in processed:
                 processed.append(frame_id)
                 video_writer.write(frame)
@@ -98,12 +98,12 @@ class Camera:
         return file
 
     def motion_detection(self):
-        self.surveillance_mode = True
+        self._surveillance_mode = True
         previous_frame = None
         last_frame_id = 0
 
-        while self.surveillance_mode:
-            frame_id, frame = self.camera.read()
+        while self._surveillance_mode:
+            frame_id, frame = self._camera.read()
             if frame_id == last_frame_id:
                 continue
             last_frame_id = frame_id
@@ -146,7 +146,7 @@ class Camera:
             picture_seconds=5
     ) -> Generator[Dict[str, Any], None, None]:
         status = Camera.STATUS_IDLE
-        fps = self.camera.fps
+        fps = self._camera.fps
         processed = []
         n_frames = 0
         file: Optional[IO] = None
@@ -178,7 +178,7 @@ class Camera:
                     status = Camera.STATUS_IDLE
 
     def surveillance_stop(self):
-        self.surveillance_mode = False
+        self._surveillance_mode = False
 
     def _create_video_file(self, event_type: str) -> Tuple[IO, cv2.VideoWriter]:
         now_str = str(datetime.now())[:-7]
@@ -189,7 +189,7 @@ class Camera:
         writer = cv2.VideoWriter(
             file.name,
             cv2.VideoWriter_fourcc(*'mp4v'),
-            self.camera.fps,
-            self.camera.frame_size
+            self._camera.fps,
+            self._camera.frame_size
         )
         return file, writer
