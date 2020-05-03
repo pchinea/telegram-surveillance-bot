cf66ac840da9572191c062e17ae853fdea0f8040
Pablo Chinea <khertz@gmail.com>
2020-05-03 16:28:26 +0100

    Moves configuration into main.py.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
 """Main script for Surveillance Telegram Bot."""
+import logging
+import os
+
 from bot import Bot
-import config
+
+# Configuration environment variables.
+AUTHORIZED_USER = os.environ.get('AUTHORIZED_USER')
+BOT_API_TOKEN = os.environ.get('BOT_API_TOKEN')
+LOG_LEVEL = os.environ.get('LOG_LEVEL', logging.WARNING)
+BOT_LOG_LEVEL = os.environ.get('BOT_LOG_LEVEL', None)
+
+
+# Logging config.
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=LOG_LEVEL
+)
 
 
 def main():
     """Surveillance Telegram Bot start up function."""
-    bot = Bot(token=config.BOT_API_TOKEN, username=config.AUTHORIZED_USER,
-              log_level=config.APP_LOG_LEVEL)
+    bot = Bot(token=BOT_API_TOKEN, username=AUTHORIZED_USER,
+              log_level=BOT_LOG_LEVEL)
     bot.start()
 
 
