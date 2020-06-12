39ef6a20d6e8d657849e4580b5c456c7e8256eaf
Pablo Chinea <khertz@gmail.com>
2020-06-12 22:24:15 +0100

    Implements test for launch script.

---
+++ tests/test_launch_script.py
@@ -0,0 +1,26 @@
+"""
+Test suite for launch script testing.
+"""
+import logging
+import runpy
+
+import _pytest.logging
+import pytest
+
+
+def test_launch_script(caplog: _pytest.logging.caplog):
+    """
+    Tests launch script invocation.
+
+    Args:
+        caplog: Fixture for log messages capturing.
+    """
+    with pytest.raises(SystemExit) as error:
+        runpy.run_path('surveillance_bot.py')
+
+    assert len(caplog.records) == 1
+    record = caplog.records[0]
+    assert record.levelno == logging.CRITICAL
+    assert 'BOT_API_KEY' in record.message
+    assert error.type == SystemExit
+    assert error.value.code == 1
