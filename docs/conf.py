91e7b3582bb17421e54db460295ef0e23854abfa
Pablo Chinea <khertz@gmail.com>
2020-06-13 17:53:01 +0100

    Fixes docs config.

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../surveillance_bot'))
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
